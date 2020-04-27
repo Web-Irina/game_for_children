4866de592a85be4ff1229e31da11c49382b60886
Web-Irina <ichetvergova1105@gmail.com>
2020-04-27 11:34:20 +0300

    lesson 3

--- main.py
+++ main.py
@@ -8,31 +8,47 @@ pygame.display.set_caption('score and multiplication table')
 
 #координаты игрока, которые можно регулировать
 x = 50
-y = 50
-whdht = 40
+y = 425
+width = 40
 height = 60
 speed = 5
 
+isJump = False
+jumpCount = 10
+
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(35)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > 5:
         x -= speed
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < 500 - width - 5:
         x += speed
-    if keys[pygame.K_UP]:
-        y -= speed
-    if keys[pygame.K_DOWN]:
-        y += speed
+    if not (isJump):
+        if keys[pygame.K_UP] and y > 5:
+            y -= speed
+        if keys[pygame.K_DOWN] and y < 500 - height - 15:
+            y += speed
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            if jumpCount < 0:
+                y += (jumpCount ** 2) / 2
+            else:
+                y -= (jumpCount ** 2) / 2
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
 
     win.fill((0,0,0))
-    pygame.draw.rect(win, (255,255,0), (x, y, whdht, height))
+    pygame.draw.rect(win, (163,73,164), (x, y, width, height))
     pygame.display.update()
 
 pygame.quit()
